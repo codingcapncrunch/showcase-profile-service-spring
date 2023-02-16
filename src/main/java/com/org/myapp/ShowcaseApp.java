86272cd82e67f5eb36ae3b528c3f22a2584fdd64
Michael Allen Lozano <michaelalozano@Michaels-MacBook-Air.local>
2023-02-15 19:31:57 -0600

    Lozano: removing angular app and making a pure backend service

---
+++ src/main/java/com/org/myapp/ShowcaseApp.java
@@ -0,0 +1,13 @@
+package com.org.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/myapp/ShowcaseAppTests.java
@@ -0,0 +1,13 @@
+package com.org.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
